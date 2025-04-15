6bd4526db535c1c68017ed503558367adea26f78
fengyang <yueny09@163.com>
2025-04-15 08:53:47 +0800

    rule

---
+++ calcite-rule/src/main/java/com/whosly/calcite/rule/Main.java
@@ -0,0 +1,10 @@
+package com.whosly.calcite.rule;
+
+/**
+* @author fengyang <deep_blue_yang@126.com>
+* @date on 2025/4/14
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
